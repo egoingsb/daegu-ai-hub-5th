1a341c51e450216289d1c80e899a2bdd6dc6dbd3
egoing <egoing@gmail.com>
2023-07-04 19:16:10 +0900

    복습

---
+++ 0704/trans.py
@@ -0,0 +1,22 @@
+import gradio as gr
+
+ko2enDict = {"사랑": "love", "사자": "lion", "사과": "apple"}
+en2koDict = {"love": "사랑", "lion": "사자", "apple": "사과"}
+dict = {"ko2en": ko2enDict, "en2ko": en2koDict}
+
+
+def trans(type, word):
+    return dict[type][word]
+
+
+app = gr.Interface(
+    fn=trans,
+    inputs=[
+        gr.Radio(choices=["ko2en", "en2ko"]),
+        gr.Textbox(value="사랑", info="한국어를 입력하세요"),
+    ],
+    outputs="text",
+    examples=[["ko2en", "사랑"], ["en2ko", "love"]],
+    allow_flagging="manual",
+)
+app.launch(debug=True)
