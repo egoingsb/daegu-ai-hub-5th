ce4cbe39a490a0b5801fce675e9cdbe5b392fb08
egoing <egoing@gmail.com>
2023-08-03 20:16:37 +0900

    기본 골격 구현

--- 0803/server.py
+++ 0803/server.py
@@ -6,12 +6,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return f"""
-      <h1>Python</h1>
-      {random.random()}
-      <h1>JavaScript</h1>
-      <script>
-        document.write(Math.random());
-      </script>
+    <html>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+        </ol>
+        <h2>Welcome</h2>
+        Hello, WEB!
+        <ul>
+          <li><a href="/create">create</a></li>
+        </ul>
+      </body>
+    </html>
     """
 
 app.run(debug=True)
