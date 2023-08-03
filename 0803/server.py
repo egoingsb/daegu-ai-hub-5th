ce3b0dd92297e185069eb40342e42e3ea473b2e2
egoing <egoing@gmail.com>
2023-08-03 21:20:34 +0900

    상속

--- 0803/server.py
+++ 0803/server.py
@@ -1,62 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return f"""
-    <html>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-        </ol>
-        <h2>Welcome</h2>
-        Hello, WEB!
-        <ul>
-          <li><a href="/create">create</a></li>
-        </ul>
-      </body>
-    </html>
-    """
+    return render_template('index.html')
 
 @app.route("/create")
 def create():
-    return f"""
-    <html>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-        </ol>
-        <h2>Create</h2>
-        create!!
-        <ul>
-          <li><a href="/create">create</a></li>
-        </ul>
-      </body>
-    </html>
-    """
+    return render_template('create.html')
 
 @app.route("/read/<id>")
 def read(id):
-    return f"""
-    <html>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-        </ol>
-        <h2>Read</h2>
-        Read!! {id}
-        <ul>
-          <li><a href="/create">create</a></li>
-        </ul>
-      </body>
-    </html>"""
+    return render_template('read.html')
 
 app.run(debug=True)
