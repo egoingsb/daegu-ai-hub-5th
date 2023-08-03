b83fd1d7cfc7f8d4d01dd62681113f066f6088f2
egoing <egoing@gmail.com>
2023-08-03 21:49:50 +0900

    템플릿에 데이터 주입하기

--- 0803/server.py
+++ 0803/server.py
@@ -3,16 +3,21 @@ import random
 
 app = Flask(__name__)
 
+topics = [
+    {"id":1, "title":"html", "body":"html is ..."},
+    {"id":2, "title":"css", "body":"css is ..."}
+]
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', topics=topics)
 
 @app.route("/create")
 def create():
-    return render_template('create.html')
+    return render_template('create.html', topics=topics)
 
 @app.route("/read/<id>")
 def read(id):
-    return render_template('read.html')
+    return render_template('read.html', topics=topics)
 
 app.run(debug=True)
