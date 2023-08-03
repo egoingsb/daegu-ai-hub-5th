e4d61f57b4e12cadd711e0cd051655d5745a4e4d
egoing <egoing@gmail.com>
2023-08-03 20:48:33 +0900

    라우팅 작업

--- 0803/server.py
+++ 0803/server.py
@@ -22,4 +22,41 @@ def index():
     </html>
     """
 
+@app.route("/create")
+def create():
+    return f"""
+    <html>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+        </ol>
+        <h2>Create</h2>
+        create!!
+        <ul>
+          <li><a href="/create">create</a></li>
+        </ul>
+      </body>
+    </html>
+    """
+
+@app.route("/read/<id>")
+def read(id):
+    return f"""
+    <html>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+        </ol>
+        <h2>Read</h2>
+        Read!! {id}
+        <ul>
+          <li><a href="/create">create</a></li>
+        </ul>
+      </body>
+    </html>"""
+
 app.run(debug=True)
