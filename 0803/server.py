58e87073226ef576da2d53b90c60aa7ba244fd15
egoing <egoing@gmail.com>
2023-08-04 19:18:40 +0900

    생성 기능 구현

--- 0803/server.py
+++ 0803/server.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import random
 
 app = Flask(__name__)
 
+nextId = 4
 topics = [
     {"id":1, "title":"html", "body":"html is ..."},
     {"id":2, "title":"css", "body":"css is ..."}, 
@@ -17,6 +18,12 @@ def index():
 def create():
     return render_template('create.html', topics=topics)
 
+@app.route("/create_process", methods=['POST'])
+def create_process():
+    global nextId
+    topics.append({"id":nextId, "title":request.form['title'], "body":request.form['body']})
+    nextId = nextId + 1
+    return redirect(f'/read/{nextId-1}')
 @app.route("/read/<int:id>")
 def read(id):
     selected = None
