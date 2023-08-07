0916bc7c63c07941a056fcfe0562fc8875992132
egoing <egoing@gmail.com>
2023-08-07 18:19:08 +0900

    시멘틱 검색 기능 구현

--- 0803/server.py
+++ 0803/server.py
@@ -3,6 +3,9 @@ import psycopg2
 from pgvector.psycopg2 import register_vector
 import numpy as np
 
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
+
 app = Flask(__name__)
 
 conn = psycopg2.connect(
@@ -27,7 +30,6 @@ topics = [
 def index():
     cursor.execute("SELECT id, title FROM topics")
     topics = cursor.fetchall()
-    print(topics)
     return render_template('index.html', topics=topics)
 
 @app.route("/create")
@@ -36,11 +38,39 @@ def create():
     topics = cursor.fetchall()
     return render_template('create.html', topics=topics)
 
+@app.route("/search")
+def search():
+    cursor.execute("SELECT id, title FROM topics")
+    topics = cursor.fetchall()
+    
+    query = request.args.get('query')
+    import openai
+    response = openai.Embedding.create(
+        input=query,
+        model="text-embedding-ada-002"
+    )
+    embeddings = np.array(response['data'][0]['embedding'])
+    cursor.execute("SELECT id, title, body FROM topics ORDER BY embedding <=> %s", (embeddings,))
+    searched = cursor.fetchall()
+    print('searched', searched)
+    return render_template('search.html', topics=topics, searched=searched)
+
 @app.route("/create_process", methods=['POST'])
 def create_process():
     t = request.form['title']
     b = request.form['body']
-    cursor.execute("INSERT INTO topics (title, body) VALUES(%s,%s) RETURNING id", (t, b))
+    import openai
+    response = openai.Embedding.create(
+        input=f'''title:{t}\n\nbody:{b}''',
+        model="text-embedding-ada-002"
+    )
+    embeddings = np.array(response['data'][0]['embedding'])
+    cursor.execute("""
+        INSERT INTO topics 
+            (title, body, embedding) 
+        VALUES(%s,%s,%s) 
+            RETURNING id""", 
+        (t, b, embeddings))
     lastid = cursor.fetchone()[0]
     conn.commit()
     return redirect(f'/read/{lastid}')
